4c1fd6f4897ed09dbf227ff165b6da219c0f4dd6
saccuz <marco.sacchet.97@gmail.com>
2023-05-31 17:14:53 +0200

    Added framework tests

--- test/microgp4/framework/test_framework.py
+++ test/microgp4/framework/test_framework.py
@@ -16,8 +16,34 @@ import pytest
 import microgp4 as ugp
 
 def test_alternative():
-    """uno tra vari"""
-    pass
+    p1 = ugp.f.integer_parameter(0,4)
+    m1 = ugp.f.macro("test number {p1}", p1=p1)
+    m2 = ugp.f.macro("test number {p2}", p2=p1)
+    m3 = ugp.f.macro("test number {p3}", p3=p1)
+    a1 = ugp.f.alternative([m1,m2,m3])
+    a2 = ugp.f.alternative([m1,m2])
+    a3 = ugp.f.alternative([m1,m3])
+
+    a = a1()
+    b = a1()
+    c = a2()
+    d = a3()
+
+    assert a != None
+    assert a == b
+    assert a.successors[0] == m1
+    assert a.successors[0] in [m2,m3]
+
+    assert a != c
+    assert a != d
+    assert c != d
+
+    with pytest.raises(ugp.user_messages.MicroGPError):
+        ugp.f.alternative(12)
+    with pytest.raises(ugp.user_messages.MicroGPError):
+        ugp.f.alternative('abc')
+    with pytest.raises(ugp.user_messages.MicroGPError):
+        ugp.f.alternative(p1)
 
 def test_sequence():
     t1 = 'abc'
@@ -51,5 +77,28 @@ def test_sequence():
         ugp.f.sequence(12)
 
 def test_bunch():
-    """tanti tra vari (o uno)"""
-    pass
+    p1 = ugp.f.integer_parameter(0,4)
+    m1 = ugp.f.macro("test number {p1}", p1=p1)
+    m2 = ugp.f.macro("test number {p2}", p2=p1)
+    m3 = ugp.f.macro("test number {p3}", p3=p1)
+    b1 = ugp.f.bunch(m1, (2,4))
+    b2 = ugp.f.bunch(m1, (2,5))
+    b3 = ugp.f.bunch(m1)
+    b4 = ugp.f.bunch([m1,m2,m3], (4,7))
+
+    a = b1()
+    b = b1()
+    c = b2()
+    d = b3()
+    e = b4()
+
+    assert a is not None
+    assert len(a.successors) in [2,3]
+    assert a.successors[0] == m1
+    assert a == b
+    assert a != c
+    assert len(d.successors) == 1
+    assert all(i in [m1,m2,m3] for i in e.successors)
+
+    with pytest.raises(ugp.user_messages.MicroGPError):
+        ugp.f.bunch(p1)
