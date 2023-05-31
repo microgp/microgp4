d4ab8eae56aca02c080b60a42ec8c182ba35fd34
Marco Sacchet <marco.sacchet.97@gmail.com>
2023-05-31 13:46:45 +0200

    FIx name import

--- test/microgp4/framework/test_macro.py
+++ test/microgp4/framework/test_macro.py
@@ -12,13 +12,13 @@
 # Copyright 2022-23 Giovanni Squillero and Alberto Tonda
 # SPDX-License-Identifier: Apache-2.0
 
-import microgp4 as ugp4
+import microgp4 as ugp
 
 def test_macro():
-    p1 = ugp4.f.integer_parameter(0,4)
-    m1 = ugp4.f.macro("test number {p1}", p1=p1)
-    m2 = ugp4.f.macro("test numbers {p1}", p1=p1)
-    m3 = ugp4.f.macro("test number {p2}", p2=p1)
+    p1 = ugp.f.integer_parameter(0,4)
+    m1 = ugp.f.macro("test number {p1}", p1=p1)
+    m2 = ugp.f.macro("test numbers {p1}", p1=p1)
+    m3 = ugp.f.macro("test number {p2}", p2=p1)
     a = m1()
     b = m1()
     c = m2()
@@ -33,7 +33,7 @@ def test_macro():
     assert list(a.parameter_types.keys()) == ['p1']
     assert a.text == "test number {p1}"
 
-    # graph.py, ln 175-176
+    # graph.py, mimic _unroll_macro, ln 125,126
     for k, p in a.parameter_types.items():
         a.parameters[k] = p()
 
