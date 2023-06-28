d5d0b64b4c40fc5da576bd91b0896689d232cd86
Zyad Barakat <zyad9@outlook.com>
2023-06-28 03:34:30 +0300

    trying to test Individual Class

--- test/microgp4/classes/test_individual.py
+++ test/microgp4/classes/test_individual.py
@@ -22,6 +22,90 @@
 # from microgp4.classes.individual import Individual
 # import networkx as nx
 
+from typing import Any, Type
+
+import microgp4 as ugp
+import networkx as nx
+
+NODE_ZERO = 0
+FRAMEWORK = "framework"
+
+def test_individual():
+    
+    G1 = nx.MultiDiGraph()
+    G2 = nx.MultiDiGraph()
+
+    G1.add_node(NODE_ZERO, data='test')
+    G2.add_node(NODE_ZERO, data='test')
+
+
+    ind1 = ugp.classes.Individual(G1)
+    ind2 = ugp.classes.Individual(G2)
+
+    print(ind1)
+    assert ind1 == ind2
+    assert ind1 != None
+
+    macro1a = ugp.f.macro('1')
+    macro1b = ugp.f.macro('call {m1}', m1=ugp.f.global_reference('frame1'))
+    frame1 = ugp.f.bunch([macro1a, macro1a, macro1a, macro1a, macro1b], name='frame1', size=10_000)
+    frame2 = ugp.f.bunch([macro1a], name='frame2', size=10_000)
+    ind = ugp.classes.Individual(top_frame=frame1)
+
+    ind.G.add_edge(0, 1, kind=FRAMEWORK)
+    ind.G.add_node(1, _macro=macro1a)
+    ind.G.add_node(0, _frame=frame2)
+    print(len(ind.macros))
+
+    assert str(ind.grammar_tree) == "DiGraph with 2 nodes and 1 edges"
+    assert ind != ind1
+    
+    assert ind1.fitness == None
+    assert ind2.fitness == None
+
+    ind1.fitness = 42
+    ind2.fitness = 43
+
+    assert ind1.fitness == 42
+    assert ind2.fitness == 43
+
+    ind2.fitness = 42   
+
+    assert ind1.fitness == ind2.fitness
+    assert ind1.genome != ind2.genome
+
+    print(ind.dump)
+
+    assert len(ind.macros) == 2 # why not 1?
+
+    assert ind.discard_useless_components() == None
+
+    ind.G.add_edge(0, 2, kind=FRAMEWORK)
+    ind.G.add_node(2, _macro=macro1b)
+
+    assert len(ind.macros) == 3
+    #print(ind.as_lgp()) # what to do?
+
+    #print(ind.check())
+    #print(ind.parameters)
+
+    # G1 = nx.MultiDiGraph()
+    # G1.add_edge(0, 1, kind=FRAMEWORK)
+
+    # ind1 = ugp.classes.Individual(top_frame=G1)
+    # print(ind1.G.nodes(data=True))
+    # print("aywa")
+    # tree = ind1.grammar_tree()
+
+    # expected_tree = nx.DiGraph()
+    # expected_tree.add_edge(1, 2)
+    # expected_tree.add_edge(2, 3)
+
+    # assert nx.is_isomorphic(tree, expected_tree)
+
+    # assert nx.is_branching(tree) and nx.is_weakly_connected(tree)
+
+
 # # def test_individual_creation():
 # #     G = nx.MultiDiGraph()
  
@@ -36,15 +120,7 @@
 # #     ind1 = Individual(top_frame=G)
 # #     ind2 = Individual(top_frame=G)
  
-# #     assert ind1.fitness == None
-# #     assert ind2.fitness == None
-# #     ind1.fitness = 42
-# #     ind2.fitness = 43
-# #     assert ind1.fitness == 42
-# #     assert ind2.fitness == 43
-# #     ind2.fitness = 42   
-# #     assert ind1.fitness == ind2.fitness
-# #     assert ind1.genome != ind2.genome
+
 # #     my_dict = {}
  
 # #     G = nx.MultiDiGraph()
