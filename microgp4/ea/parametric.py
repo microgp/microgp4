15f376b7bcbe7f83f356c33cafa7d4c04fef74d0
Marco Sacchet <marco.sacchet.97@gmail.com>
2023-07-12 19:26:40 +0200

    Fix typo in vanilla_ea FIrst parametric_ea

--- microgp4/ea/parametric.py
+++ microgp4/ea/parametric.py
@@ -25,9 +25,9 @@
 # limitations under the License.
 
 # =[ HISTORY ]===============================================================
-# v1 / April 2023 / Squillero (GX)
+# v1 / July 2023 / Squillero (GX)
 
-__all__ = ['vanilla_ea']
+__all__ = ['parametric_ea']
 
 from microgp4.operators import *
 from microgp4.sys import *
@@ -40,20 +40,22 @@ from microgp4.user_messages import *
 
 from .selection import *
 
-
 def _new_best(population: Population, evaluator: EvaluatorABC):
     microgp_logger.info(
         f"VanillaEA: 🍀 {population[0].describe(include_fitness=True, include_structure=False, include_birth=False)}" +
         f" [🕓 gen: {population.generation:,} / fcalls: {evaluator.fitness_calls:,}]")
 
-
-def vanilla_ea(top_frame: type[FrameABC],
+def parametric_ea(top_frame: type[FrameABC],
                evaluator: EvaluatorABC,
                mu: int = 10,
                lambda_: int = 20,
                max_generation: int = 100,
-               max_fitness: FitnessABC | None = None) -> Population:
-    r"""A simple evolutionary algorithm
+               max_fitness: FitnessABC | None = None,
+               top_best: int = None,
+               lifespan: int = None,
+               operators: list = None) -> Population:
+    
+    r"""A configurable evolutionary algorithm
 
     Parameters
     ----------
@@ -65,62 +67,18 @@ def vanilla_ea(top_frame: type[FrameABC],
         The size of the population
     lambda_
         The size the offspring
+    top_best
+        The size of champions population
+    lifespan
+        The number of generation an individual survive
 
     Returns
     -------
     Population
         The last population
 
-    """
-    #SElement.is_valid = SElement._is_valid_debug
+    """ 
     population = Population(top_frame)
-
-    # Initialize population
-    ops0 = [op for op in get_operators() if op.num_parents is None]
-    gen0 = list()
-    while len(gen0) < mu:
-        o = rrandom.choice(ops0)
-        gen0 += o(top_frame=top_frame)
-    population += gen0
-    evaluator(population)
-    population.sort()
-    best = population[0]
-    _new_best(population, evaluator)
-
-    pass
-
-    all_individuals = set()
-
-    stopping_conditions = list()
-    stopping_conditions.append(lambda: population.generation >= max_generation)
-    if max_fitness:
-        stopping_conditions.append(lambda: best.fitness == max_fitness or best.fitness >> max_fitness)
-
-    # Let's roll
-    while not any(s() for s in stopping_conditions):
-        ops = [op for op in get_operators() if op.num_parents is not None]
-        new_individuals = list()
-        for step in range(lambda_):
-            op = rrandom.choice(ops)
-            parents = list()
-            for _ in range(op.num_parents):
-                parents.append(tournament_selection(population, 1))
-            new_individuals += op(*parents)
-
-        population += new_individuals
-        evaluator(population)
-        population.sort()
-
-        all_individuals |= set(population)
-
-        population.individuals[mu:] = []
-
-        if best.fitness << population[0].fitness:
-            best = population[0]
-            _new_best(population, evaluator)
-
-    #population._zap = all_individuals
-    microgp_logger.info("VanillaEA: Genetic operators statistics:")
-    for op in get_operators():
-        microgp_logger.info(f"VanillaEA: * {op.__qualname__}: {op.stats}")
-    return population
+    if operators is None or all(op.num_parents is not None for op in operators):
+        ops0 = [op for op in get_operators() if op.num_parents is None]
+    
